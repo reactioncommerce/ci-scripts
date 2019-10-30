8d719be3b4aa746cf3ef5b470878309de789e1a5
Wiston Mwinzi <wiston.mwinzi@Wistons-MacBook-Pro.local>
2019-10-30 16:45:42 +0300

    added double quote to prevent globbing
    Signed-off-by: Wiston Mwinzi <wiston.mwinzi@Wistons-MacBook-Pro.local>

--- create-gitops-pull-request.sh
+++ create-gitops-pull-request.sh
@@ -13,38 +13,38 @@ set -o posix    # more strict failures in subshells
 IFS=$'\n\t'
 # ---- End unofficial bash strict mode boilerplate
 
-if [ -z $SERVICE ]; then
+if [ -z "$SERVICE" ]; then
   echo "Please set the SERVICE environment variable i.e hydra, keto etc"
   exit 1
 fi
 
-if [ -z $ENVIRONMENT ]; then
+if [ -z "$ENVIRONMENT" ]; then
   echo "Please set the ENVIRONMENT environment variable i.e dev, staging etc"
   exit 1
 fi
 
 
-if [ -z $DOCKER_REPOSITORY ]; then
+if [ -z "$DOCKER_REPOSITORY" ]; then
   echo "Please set the DOCKER_REPOSITORY environment variable i.e dev, staging etc"
   exit 1
 fi
 
-if [ -z $REACTION_GITOPS_GH_TOKEN ]; then
+if [ -z "$REACTION_GITOPS_GH_TOKEN" ]; then
   echo "Please set the REACTION_GITOPS_GH_TOKEN environment variable."
   exit 1
 fi
 
-if [ -z $GH_USERNAME ]; then
+if [ -z "$GH_USERNAME" ]; then
   echo "Please set the GH_USERNAME environment variable."
   exit 1
 fi
 
-if [ -z $GH_EMAIL ]; then
+if [ -z "$GH_EMAIL" ]; then
   echo "Please set the GH_EMAIL environment variable."
   exit 1
 fi
 
-if [ -z $REACTION_GITOPS_REVIEWERS ]; then
+if [ -z "$REACTION_GITOPS_REVIEWERS" ]; then
   echo "Please set the REACTION_GITOPS_REVIEWERS environment variable."
   exit 1
 fi
@@ -53,26 +53,24 @@ fi
 HUB=/usr/local/bin/hub
 
 # Clone reaction-gitops repo and configure username and email for signing off commits
-"${HUB}" clone https://${REACTION_GITOPS_GH_TOKEN}@github.com/reactioncommerce/reaction-gitops.git
+"${HUB}" clone https://"${REACTION_GITOPS_GH_TOKEN}"@github.com/reactioncommerce/reaction-gitops.git
 cd reaction-gitops
 "${HUB}" config user.name "${GH_USERNAME}"
 "${HUB}" config user.email "${GH_EMAIL}"
-cd kustomize/${SERVICE}/overlays/${ENVIRONMENT}
+cd kustomize/${SERVICE}/overlays/"${ENVIRONMENT}"
 
 # Create new branch
-"${HUB}" checkout -b update-image-${SERVICE}-${CIRCLE_SHA1}
+"${HUB}" checkout -b update-image-"${SERVICE}"-"${CIRCLE_SHA1}"
 
 # Modify image tag in kustomization.yaml by calling 'kustomize edit set image'
-"${HUB}" edit set image docker.io/${DOCKER_REPOSITORY}:${CIRCLE_SHA1}
+"${HUB}" edit set image docker.io/"${DOCKER_REPOSITORY}":"${CIRCLE_SHA1}"
 "${HUB}" add kustomization.yaml
 
 # Commit with sign-off
-"${HUB}" commit -s -m "changed ${SERVICE} image tag to ${CIRCLE_SHA1}"
+"${HUB}" commit -s -m "changed "${SERVICE}" image tag to "${CIRCLE_SHA1}"
 
 # Push branch to origin
-"${HUB}" push --set-upstream origin update-image-${SERVICE}-${CIRCLE_SHA1}
+"${HUB}" push --set-upstream origin update-image-"${SERVICE}"-"${CIRCLE_SHA1}"
 
 # Create PR
-"${HUB}" pull-request --no-edit -r ${REACTION_GITOPS_REVIEWERS}
-
-
+"${HUB}" pull-request --no-edit -r "${REACTION_GITOPS_REVIEWERS}"
