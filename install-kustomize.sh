c807835f77159114f7c92db0a6c7750f83431cf2
Wiston Mwinzi <wiston.mwinzi@Wistons-MacBook-Pro.local>
2019-10-31 22:52:39 +0300

    need to pass the version
    Signed-off-by: Wiston Mwinzi <wiston.mwinzi@Wistons-MacBook-Pro.local>

--- install-kustomize.sh
+++ install-kustomize.sh
@@ -13,7 +13,10 @@ set -o posix    # more strict failures in subshells
 IFS=$'\n\t'
 # ---- End unofficial bash strict mode boilerplate
 
-KUSTOMIZE_VERSION=3.2.1
+if [ -z $KUSTOMIZE_VERSION ]; then
+  echo Please set the KUSTOMIZE_VERSION environment variable.
+  exit 1
+fi
 
 # Download kustomize
 cd "$(mktemp -d /tmp/kustomize-XXX)"
